d49fa9a5cbe4359718ecdf2981f402b737af0784
김현규 <46562138+codeFabian@users.noreply.github.com>
2022-01-13 14:16:29 +0900

    Time: 128 ms (41.59%), Space: 41.6 MB (35.26%) - LeetHub

---
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+5 */
+var removeDuplicates = function(nums) {
+    let i = 0;
+    for (let j = 0; j < nums.length; j++) {
+        if (nums[j] != nums[i]) 
+            nums[++i] = nums[j];
+    }
+    return ++i;
+};
